b5508906a148c322ecd6ba0740ad21e5ff886505
Martina Marino <45880539+martimarino@users.noreply.github.com>
2024-03-07 19:16:06 +0100

    Delete tinyDNNAlt/src/tinyDNNAlt_MPI directory

--- tinyDNNAlt/src/tinyDNNAlt_MPI/tiny_dnn/models/models.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-	namespace models {
-		#include "tiny_dnn/models/alexnet.h"
-		#include "tiny_dnn/models/vgg16.h"
-		#include "tiny_dnn/models/vgg19.h"
-		#include "tiny_dnn/models/resnet34.h"
-		#include "tiny_dnn/models/resnet152.h"
-	}
