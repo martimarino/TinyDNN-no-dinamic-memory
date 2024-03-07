ba5492f4f47c43f8ca270993e692f79316291d71
rmatrella <r.matrella@studenti.unipi.it>
2024-03-07 19:21:59 +0100

    substituting etl vector

---
+++ tinyDNNAlt/src/tinyDNNAlt_vanilla/tiny_dnn/util/parameters.h
@@ -0,0 +1,6 @@
+#define MAX_INPUT_SIZE 1024 // considering 32 * 32
+#define MAX_OUTPUT_SIZE 1000
+#define MAX_LABEL_SIZE 100
+#define MAX_LAYERS 20
+#define MAX_NODES 2560 // 20 * 128
+#define MAX_TENSOR_SIZE 3072    // considering 32 * 32 * 3
