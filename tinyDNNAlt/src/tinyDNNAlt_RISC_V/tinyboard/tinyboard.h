6c07a7aa6b9086e96e7e151c5ee2fe6e862d5ffa
Martina Marino <45880539+martimarino@users.noreply.github.com>
2024-03-07 19:16:13 +0100

    Delete tinyDNNAlt/src/tinyDNNAlt_RISC_V directory

--- tinyDNNAlt/src/tinyDNNAlt_RISC_V/tinyboard/tinyboard.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace tinyboard {
-	const std::vector<std::string> topics{
-		"/tmp/tinyboard.topics.epochs",
-		"/tmp/tinyboard.topics.accuracy"
-	};
-}
-#include "tinyboard/pipehandler.h"
-#include "tinyboard/broker.h"
